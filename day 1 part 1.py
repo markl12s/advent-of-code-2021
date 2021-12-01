b9dbb482decd6103f1e98c1a31e63e45aaf665a1
Mark <72723296+markl12s@users.noreply.github.com>
2021-12-01 00:53:42 -0600

    Create day 1 part 1.py

---
+++ day 1 part 1.py
@@ -0,0 +1,10 @@
+measurments = measurment_string.split("\n")
+print(measurments)
+
+increases = 0
+
+for i in range(len(measurments) - 1):
+    if measurment[i] < measurment[i + 1]:
+        increases += 1
+
+print(increases)
