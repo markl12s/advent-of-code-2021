9ba9428498a7a2374f9bcd838ed868de62e48bef
Mark <72723296+markl12s@users.noreply.github.com>
2021-12-01 00:54:13 -0600

    Create day 1 part 2.py

---
+++ day 1 part 2.py
@@ -0,0 +1,13 @@
+measurments = measurment_string.split("\n")
+print(measurments)
+
+increases = 0
+
+for i in range(len(measurments) - 3):
+    window_a = int(measurments[i]) + int(measurments[i + 1]) + int(measurments[i + 2])
+    window_b = int(measurments[i + 1]) + int(measurments[i + 2]) + int(measurments[i + 3])
+
+    if window_a < window_b:
+        increases += 1
+
+print(increases)
